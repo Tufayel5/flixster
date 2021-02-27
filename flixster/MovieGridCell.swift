68773c5dd0050c78bd3dc12d84eba9b985c19dc8
Tufayel Ahmed <tufayel5@gmail.com>
2021-02-27 15:02:39 -0500

    Flix Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Tufayel Ahmed on 2/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
